8bd190da2332fcdc884662a6ea64e1d3bf6a6825
xiangtao <xiangtao1994@gmail.com>
2025-05-25 16:49:32 +0800

    feat： panic打印到日志

--- pkg/middleware/recover.go
+++ pkg/middleware/recover.go
@@ -1,7 +1,14 @@
 package middleware
 
 import (
+	"errors"
+	"github.com/xiangtao94/golib/pkg/zlog"
+	"go.uber.org/zap"
+	"net"
 	"net/http"
+	"os"
+	"runtime/debug"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +19,41 @@ func RegistryRecovery(engine *gin.Engine, handle gin.RecoveryFunc) {
 			c.AbortWithStatus(http.StatusInternalServerError)
 		}
 	}
-	engine.Use(gin.CustomRecovery(handle))
+	engine.Use(CustomRecoveryWithZap(zlog.NewLoggerWithSkip(1), handle))
+}
+
+func CustomRecoveryWithZap(logger *zap.Logger, handle gin.RecoveryFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					var se *os.SyscallError
+					if errors.As(ne, &se) {
+						seStr := strings.ToLower(se.Error())
+						if strings.Contains(seStr, "broken pipe") ||
+							strings.Contains(seStr, "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+				if brokenPipe {
+					logger.Error("Broken pipe or connection reset by peer",
+						zap.Any("error", err),
+					)
+					c.Error(err.(error)) // 记录 gin 错误
+					c.Abort()
+					return
+				}
+				logger = zlog.LoggerWithContext(logger, c)
+				// 正常 panic 情况
+				logger.Error("Panic Recovery",
+					zap.Any("error", err),
+					zap.Any("stack", string(debug.Stack())),
+				)
+				handle(c, err)
+			}
+		}()
+		c.Next()
+	}
 }
